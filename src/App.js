44148275b9414599b913e256997b6f0d4a6d7bb5
Alon Joshua <alonjoshua@gmail.com>
2021-04-22 18:12:37 +0300

    styling menu item component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Homepage from './Homepage.component'
+import Homepage from './pages/homepage/Homepage.component'
 
 class App extends Component {
   render() {

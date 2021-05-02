e2f70fcb9304e5661a4e1e1732582324c33b8f91
Alon Joshua <alonjoshua@gmail.com>
2021-05-02 18:46:14 +0300

    adding header and basic navigation

--- src/App.js
+++ src/App.js
@@ -4,17 +4,18 @@ import './App.css';
 
 import Homepage from './pages/homepage/Homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>        
         <Route exact path='/' component={Homepage} />
         <Route path='/shop' component={ShopPage} />
       </Switch>
-
     </div>
-  )
+  );
 }
 
 export default App;
